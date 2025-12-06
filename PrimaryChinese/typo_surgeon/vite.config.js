dc581175c0560301c170a590f35d96ba97fab657
keithsflau <keith.sf.lau@gmail.com>
2025-12-06 15:44:42 +0800

    修復 idiom-zoo-race 部署設定

--- PrimaryChinese/typo_surgeon/vite.config.js
+++ PrimaryChinese/typo_surgeon/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // 使用相對路徑，適用於 GitHub Pages 子目錄部署
   base: './',
   server: {
     port: 3002,
@@ -11,6 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    emptyOutDir: true
   }
 })
