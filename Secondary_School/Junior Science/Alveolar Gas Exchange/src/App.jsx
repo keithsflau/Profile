cf28b5e8600b4b350b4686d89832c4d06ada519e
keithsflau <keith.sf.lau@gmail.com>
2025-12-13 20:22:04 +0800

    Fix Biology apps: Clean source, Build dist, Inject counters, Update links

---
+++ Secondary_School/Junior Science/Alveolar Gas Exchange/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GasExchangeSimulator from './components/GasExchangeSimulator'
+
+function App() {
+  return (
+    <div className="App">
+      <GasExchangeSimulator />
+    </div>
+  )
+}
+
+export default App
